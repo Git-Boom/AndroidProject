b436b1181022875d7a7604ecef25cd90ba65ad4f
Git-Boom <74344529+Git-Boom@users.noreply.github.com>
2020-11-12 15:17:34 +0800

    Add files via upload

---
+++ ModuleAppDemo/app/src/main/java/com/example/moduleappdemo/AppApplication.kt
@@ -0,0 +1,9 @@
+package com.example.moduleappdemo
+
+import android.app.Application
+
+class AppApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ ModuleAppDemo/maincomponent/src/main/java/com/example/maincomponent/application/ModuleDemoApplication.kt
@@ -0,0 +1,9 @@
+package com.example.maincomponent.application
+
+import android.app.Application
+
+class ModuleDemoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
