472462ea80995e5f0327c8363c3a32cb50b2994e
Raby <rabyxie@gmail.com>
2020-11-12 17:45:25 +0800

    Revert "解决apk无法启动的问题，因为没有加载kotlin插件，导致kotlin的文件没有打包编译，所以找不到文件"
    This reverts commit a93b48fe120d94cb1c93eb71c136cdb3bbf73cf5.

--- ModuleAppDemo/maincomponent/src/main/java/com/example/moduleappdemo/application/ModuleDemoApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moduleappdemo.application
-
-import android.app.Application
-
-class ModuleDemoApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
